eaa60bc91c31f5f1d345807b24c7cabf4e9b50f3
Reneil Pascua <rpascua2@my.bcit.ca>
2018-10-14 18:00:58 -0700

    added reminder functionality

--- src/bot/DiscordBot.java
+++ src/bot/DiscordBot.java
@@ -1,16 +1,32 @@
 package bot;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.entities.ChannelType;
+import net.dv8tion.jda.core.entities.Guild;
+import net.dv8tion.jda.core.entities.Member;
+import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.MessageChannel;
+import net.dv8tion.jda.core.entities.Role;
+import net.dv8tion.jda.core.entities.TextChannel;
 import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
 public class DiscordBot extends ListenerAdapter {
 
+    Guild guild;
     boolean echo = false;
+    
+    boolean settingRem = false;
+    boolean setRemTime = false;
+    String rmd;
+    
     /**
      * log the bot in (given valid token)
      * @param args
@@ -35,21 +51,103 @@ public class DiscordBot extends ListenerAdapter {
     public void onMessageReceived(MessageReceivedEvent event) {
         
         MessageChannel channel = event.getChannel(); // returns channel of the event
-        String msg = event.getMessage().getContentDisplay(); // returns string representation of message
-        User user = event.getAuthor(); // returns user that fired event
-        Boolean isbot = user.isBot(); // true if user is a bot
+        Message message = event.getMessage();
+        String msg = message.getContentDisplay(); // returns string representation of message
+        User author = event.getAuthor(); // returns user that fired event
+        Boolean isbot = author.isBot(); // true if user is a bot
+        
+        if (event.isFromType(ChannelType.TEXT)) { // guild(server)-specific
+            guild = event.getGuild();
+            TextChannel tchannel = event.getTextChannel();
+            Member member = event.getMember(); // same as user but with guild-specific info
+            
+            List<Role> roles = new ArrayList<Role>();
+            roles = member.getRoles();
+            
+            String name;
+            if (message.isWebhookMessage()) {
+                name = author.getName(); //If this is a Webhook message, then there is no Member associated
+                                         // with the User, thus we default to the author for name.
+            } else {
+                name = member.getEffectiveName();       
+              //This will either use the Member's nickname if they have one, 
+              //otherwise it will default to their username. (User#getName())
+            }
+            
+            // prints out chatlog in console
+            System.out.printf("(%s)[%s]<%s>: %s\n", guild.getName(), tchannel.getName(), name, msg);
+        }
+        
+        // from this point on, don't do anything if the last sender was a bot.
+        if (isbot) {
+            return;
+        }
         
         // echo?
-        if (echo && !isbot) {
-            channel.sendMessage(user.getName() + " said:\n" + msg).queue();
+        if (echo) {
+            channel.sendMessage(author.getName() + " said:\n" + msg).queue();
         }
         
         // greet
-        if (msg.equalsIgnoreCase("hi") && !isbot) {
-            channel.sendMessage("hey there").queue(); // adds message to queue to send
+        if (msg.equalsIgnoreCase("hi")) {
+            greeting(channel);
         }
+        
+        // lists commands
+        if (msg.equalsIgnoreCase("!commands")) {
+            commands(channel);
+        }
+        
+        ////////////////////////////
+        // REMINDER SETTING BLOCK //
+        ////////////////////////////
+        if (setRemTime) {
+            try {
+                int length = Integer.parseInt(msg);
+                channel.sendMessage("ok, will remind you to:\n"+rmd+"\nin "+length
+                        + " minutes.").queue();
+                setRemTime = false;
+                channel.sendMessage(author.getAsMention() + "\n" + rmd + " NOW!!")
+                .queueAfter(length, TimeUnit.MINUTES);
+            } catch (Exception e) {
+                channel.sendMessage("oops, something went wrong"
+                        + "\nplease start over setting the reminder.").queue();
+                setRemTime = false;
+            }
+        }
+        
+        if (settingRem) {
+            rmd = msg;
+            setRemTime = true;
+            channel.sendMessage("in how many minutes?").queue();
+            settingRem = false;
+        }
+        
+        // starts reminder prompt
+        if (!settingRem && !setRemTime && msg.equalsIgnoreCase("set reminder")) {
+            settingRem = true;
+            channel.sendMessage("what do you want me to remind you?\n"
+                    + "ex.) take out the trash, do your homework").queue();
+        }
+        ///////// reminder part ends ////////////
+        
+        
         
         
     }
+    
+    public void greeting(MessageChannel channel) {
+        String greeting = "hello";
+        channel.sendMessage(greeting).queue();
+    }
+    
+    // helpful commands message for user
+    public void commands(MessageChannel channel) {
+        channel.sendMessage(
+                "These are the available commands:\n"
+                + "hi - prints greeting \"hey there\"")
+        .queue();
+    }
+    
     
 }
